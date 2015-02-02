1a282939c9387a8a159fc071c06ef2893c20c4aa
ferlopagu <ferlopagu@aaa7b31b-ce9c-4627-bece-64be0647eed5>
2015-02-02 16:55:31 +0000

    Add imagenes de perfil y portada en template perfil

--- trunk/badukroom/redsocial/urls.py
+++ trunk/badukroom/redsocial/urls.py
@@ -4,7 +4,7 @@ Created on 21/01/2015
 @author: fla2727
 '''
 from django.conf.urls import patterns, url
-from redsocial.views import inicio, home, crea_comentario, buscador
+from redsocial.views import perfil, home, crea_comentario, buscador
 #from django.views.generic.base import TemplateView
 
 #url(r'crea_comentario/$', crea_comentario.as_view(template_name="inicio"))
@@ -12,6 +12,6 @@ urlpatterns = patterns ('' ,
     url(r'^$', home, name='home'),
     url(r'^creaComentario/', crea_comentario, name='creaComentario'), #creaComentario coincide con la expresion regular inferior que al no encontrar username=creaComentario daba un error 404 por eso tenemos que poner antes la url de creaComentario
     url(r'^buscar/', buscador, name='buscar'),
-    url(r'^(?P<username>\w+)/$', inicio, name='inicio'),
+    url(r'^(?P<username>\w+)/$', perfil, name='perfil'),
     
 )
