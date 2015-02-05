6cc55260845217265317c3a9d60e5e57e21bf671
ferlopagu <ferlopagu@aaa7b31b-ce9c-4627-bece-64be0647eed5>
2015-02-05 16:54:06 +0000

    Solucionado: -Agregar amigo -Eliminar amigo -Aceptar peticion de amistad -Crear notificacion de peticion aceptada -Crear peticion de amistad recibida -Eliminar notificaciones y peticiones -Reestructuracion de perfil.html y home.html en base a herencia de una misma plantilla -Todas las definiciones de funciones javascript en base.html
    Falta Rechazar peticion.

--- trunk/badukroom/redsocial/urls.py
+++ trunk/badukroom/redsocial/urls.py
@@ -4,7 +4,7 @@ Created on 21/01/2015
 @author: fla2727
 '''
 from django.conf.urls import patterns, url
-from redsocial.views import perfil, home, crea_comentario, buscador, agregar_ajax, contar_notificaciones, ver_notificaciones, aceptar_peticion, declinar_peticion
+from redsocial.views import perfil, home, crea_comentario, buscador, agregar_ajax, contar_notificaciones, ver_notificaciones, aceptar_peticion, declinar_peticion, limpiar_notificaciones, eliminar_ajax
 #from django.views.generic.base import TemplateView
 
 #url(r'crea_comentario/$', crea_comentario.as_view(template_name="inicio"))
@@ -13,10 +13,11 @@ urlpatterns = patterns ('' ,
     url(r'^creaComentario/', crea_comentario, name='creaComentario'), #creaComentario coincide con la expresion regular inferior que al no encontrar username=creaComentario daba un error 404 por eso tenemos que poner antes la url de creaComentario
     url(r'^buscar/', buscador, name='buscar'),
     url(r'^agregar_ajax/', agregar_ajax, name='agregar_ajax'),
+    url(r'^eliminar_ajax/', eliminar_ajax, name='eliminar_ajax'),
     url(r'^contar_notificaciones/', contar_notificaciones, name='contar_notificaciones'),
-    url('^ver_notificaciones/', ver_notificaciones, name='ver_notificaciones'),
-    url('^aceptar_peticion/', aceptar_peticion, name='aceptar_peticion'),
-    url('^declinar_peticion/', declinar_peticion, name='declinar_peticion'),
-    url(r'^(?P<username>\w+)/$', perfil, name='perfil'),   
-    
+    url(r'^ver_notificaciones/', ver_notificaciones, name='ver_notificaciones'),
+    url(r'^aceptar_peticion/', aceptar_peticion, name='aceptar_peticion'),
+    url(r'^declinar_peticion/', declinar_peticion, name='declinar_peticion'),
+    url(r'^limpiar_notificaciones/', limpiar_notificaciones, name='limpiar_notificaciones'),
+    url(r'^(?P<username>\w+)/$', perfil, name='perfil'),     
 )
